c7564028b9856d94a3223bede23ba02f1b11d394
Vlad <vlad@example.com>
2024-09-24 16:30:11 +0300

    created models Cook, Dish, DishType, Ingredient, DishIngredient and added to gitignore **.sqlite3

--- cook/models.py
+++ cook/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.username
--- dish/models.py
+++ dish/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from cook.models import Cook
+from ingredient.models import Ingredient
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE, related_name="dishes")
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
+    ingredient = models.ManyToManyField(Ingredient, through="DishIngredient")
+
+
+class DishIngredient(models.Model):
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
