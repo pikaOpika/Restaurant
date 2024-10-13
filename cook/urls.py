9f1dd78ec7fe1fa014974998672d11268005671f
Vlad <vlad@example.com>
2024-10-13 14:09:54 +0300

    added feature(update profile)

--- cook/urls.py
+++ cook/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from cook.views import CookDetailView
+from cook.views import CookDetailView, CookUpdateView
 
 
 urlpatterns = [
-    path('profile/<int:pk>', CookDetailView.as_view(), name='profile'),
+    path('profile/<int:pk>', CookDetailView.as_view(), name='profile-detail'),
+    path('profile/update/<int:pk>', CookUpdateView.as_view(), name='profile-update')
 ]
 
 app_name = "cook"
--- cook/views.py
+++ cook/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.views import generic
 from cook.models import Cook
+from django.urls import reverse_lazy
 
 
 class CookDetailView(generic.DetailView):
     model = Cook
     template_name = "cook/cook_detail.html"
+
+
+class CookUpdateView(generic.UpdateView):
+    model = Cook
+    fields = ["years_of_experience","image", "description"]
+    
+    def get_success_url(self):
+        cook_id = self.kwargs.get("pk")
+        return reverse_lazy("cook:profile-detail", args=[cook_id])
